a7305d4252f3871eee15f3522faaf4be47a29fe6
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2020-03-27 21:00:50 -0300

    Enviando o mobile

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl'; // pacote para conversão de integer para formato moeda
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,108 @@
+import React, {useEffect, useState} from 'react';
+import { View, FlatList , Image, Text, TouchableOpacity} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import assets from '../../../assets';
+
+import styles from './styles';
+import api from '../../services/api';
+
+export default function Incidents(){
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    const [page,setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    function navigateToDetail(incident){
+        navigation.navigate('Detail', { incident });
+    }
+
+    async function loadIncidents(){
+        if(loading) {
+            return;
+        }
+
+        if(total > 0 && incidents.length == total){
+            return;
+        }
+
+        setLoading(true);
+
+
+        const response = await api.get('incidents', {
+            headers: {
+                Authentication: '849704b1',
+            },
+            params: {
+                page
+            }
+        });
+        
+        
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, [incidents]);
+
+    return (
+        <View style={ styles.container } >
+            <View style={ styles.header } >
+                <Image source={ assets.logo } />
+                <Text style={ styles.headerText } >
+                    Total de <Text style={{ fontWeight: 'bold' }}>{total} casos</Text>
+                </Text>
+            </View>
+            <Text style={styles.title}>
+                Bem-Vindo!
+            </Text>
+            <Text style={styles.description}>
+                Escolha um dos casos abaixo e salve o dia.
+            </Text>
+
+            <FlatList
+
+                data={incidents}
+                style={ styles.incidentList }
+                keyExtractor={ incident => String(incident.id) }
+                //showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
+                renderItem={({ item: incident }) => (
+                    <View style={ styles.incident }>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>
+                            {Intl.NumberFormat('pt-BR', {
+                                style:'currency',
+                                currency:'BRL' 
+                                }).format(incident.value)}
+                        </Text>
+
+                        <TouchableOpacity 
+                            style={ styles.detailsButton } 
+                            onPress={() => navigateToDetail(incident)}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
